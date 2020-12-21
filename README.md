dc0541a9c01209a3518772a42620d5f7d703c6f4
Leandro Roberto <leandro.roberto@dextra-sw.com>
2020-12-21 09:22:32 -0300

    Airbnb nodejs proj using adonis framework

---
+++ README.md
@@ -0,0 +1,17 @@
+# Adonis API application
+
+This is an implementation of the original code of Diego Fernandes from Rocketseat. 
+We are using this one just for learning purposes. 
+
+## Setup
+
+Manually clone the repo and then run `npm install`.
+
+
+### Migrations
+
+Run the following command to run startup migrations.
+
+```js
+adonis migration:run
+```
---
+++ app/Controllers/Http/PropertyController.js
@@ -0,0 +1,91 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with properties
+ */
+
+const Property = use('App/Models/Property')
+
+
+class PropertyController {
+    
+  async index({ request }) {
+    const { latitude, longitude } = request.all()
+
+    const properties = Property.query()
+      .with('images')
+      .nearBy(latitude, longitude, 10)
+      .fetch()
+
+    return properties
+  }
+  
+
+  async create ({ request, response, view }) {
+  }
+
+  async store({ auth, request, response }) {
+    const { id } = auth.user
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    const property = await Property.create({ ...data, user_id: id })
+
+    return property
+  }
+
+  
+  async show({ params }) {
+    const property = await Property.findOrFail(params.id)
+
+    await property.load('images')
+
+    return property
+  }
+
+
+  async edit ({ params, request, response, view }) {
+  }
+
+  async update({ params, request, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    property.merge(data)
+
+    await property.save()
+
+    return property
+  }
+
+
+  async destroy({ params, auth, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    if (property.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+
+    await property.delete()
+  }
+
+
+}
+
+module.exports = PropertyController
